c0851b59dce1ff2cc4e5f34a2215212647bd3b1a
Davidsolli <davidolisoa@gmail.com>
2024-10-01 15:02:15 -0300

    primeiro commit

---
+++ Back-end/src/main/java/com/pidw/sindPro/SindProApplication.java
@@ -0,0 +1,13 @@
+package com.pidw.sindPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SindProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SindProApplication.class, args);
+	}
+
+}
---
+++ Back-end/src/test/java/com/pidw/sindPro/SindProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pidw.sindPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SindProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
