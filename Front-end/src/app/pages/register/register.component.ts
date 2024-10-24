27c4f2cfd77082fa4008053244d263ec169b556a
Davidsolli <davidolisoa@gmail.com>
2024-10-24 20:19:00 -0300

    header component

---
+++ Front-end/src/app/pages/register/register.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header.component";
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss'
+})
+export class RegisterComponent {
+
+}
